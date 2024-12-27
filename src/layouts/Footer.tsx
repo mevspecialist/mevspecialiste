dcb488f10c4bd9013b31d4a5a32ad84b559cb149
firerum <ademuyiwaadewuyi@gmail.com>
2024-12-27 19:26:15 +0100

    wip

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -3,12 +3,21 @@ import React from 'react';
 import Link from 'next/link';
 import Button from '@/components/Button';
 import Image from 'next/image';
+import {
+    FaFacebook,
+    FaTwitter,
+    FaLinkedin,
+    FaInstagram,
+    FaPhone,
+    FaMapMarked,
+    FaLocationArrow,
+} from 'react-icons/fa';
 
 const socialLinks = [
-    { name: 'Facebook', href: '/' },
-    { name: 'Twitter', href: '/blog' },
-    { name: 'LinkedIn', href: '/contact' },
-    { name: 'Instagram', href: '/Services' },
+    { name: 'Facebook', href: '/', icon: FaFacebook },
+    { name: 'Twitter', href: '/blog', icon: FaTwitter },
+    { name: 'LinkedIn', href: '/contact', icon: FaLinkedin },
+    { name: 'Instagram', href: '/Services', icon: FaInstagram },
 ];
 
 const services = [
@@ -22,59 +31,95 @@ const services = [
     '24hr emergency/ambulance service',
 ];
 
+// const FooterBanner: React.FC = () => {
+//     return (
+//         <div>
+//             <h4 className="capitalize">Start Your Journey</h4>
+//             <p className="text-white">
+//                 We believe in affordable <br></br> specialty healthcare for all.
+//             </p>
+//             <Image
+//                 src="/footer-banner-lg.jpeg"
+//                 alt="Logo"
+//                 width={700}
+//                 height={50}
+//             />
+//             <Button
+//                 onClick={() => console.log('Button clicked')}
+//                 label="Shedule Appointment"
+//             />
+//         </div>
+//     );
+// };
+
 const Footer: React.FC = () => {
     return (
-        <footer className="px-6 md:text-center md:px-16 bg-footer-bg text-pry-text-color-2">
-            <div>
-                <h4 className='capitalize'>
-                    Mev specialist <br /> hospital
-                </h4>
-                <Image
-                    src="/footer-banner-lg.jpeg"
-                    alt="Logo"
-                    width={50}
-                    height={50}
-                />
-                <Button
-                    onClick={() => console.log('Button clicked')}
-                    label="Shedule Appointment"
-                />
-            </div>
-            <div>
-                {/* <img src="/path-to-logo.png" alt="Logo" /> */}
-                <Link href="/some-link">Link</Link>
-                <p>Some paragraph text here.</p>
-            </div>
-            <ul>
-                {socialLinks.map((link) => (
-                    <li key={link.name}>
-                        <a href={link.href}>{link.name}</a>
-                    </li>
-                ))}
-            </ul>
+        <footer className="px-20 py-20 md:text-center md:px-16 bg-footer-bg text-pry-text-color-2">
+            <div className="flex justify-between gap-8">
+                <div className="w-1/3">
+                    <Link href="/">
+                        <Image
+                            src="/mev-logo-text.png"
+                            width={200}
+                            height={200}
+                            alt="Logo"
+                        />
+                    </Link>
+                    <p className="text-left my-6">
+                        Mev Specialist Hospital is renowned at home and abroad
+                        for Medical excellence and attracts world-class
+                        specialist Doctors and Surgeons.
+                    </p>
+                    <ul className="flex gap-4">
+                        {socialLinks.map((link) => (
+                            <li
+                                key={link.name}
+                                className="bg-[#A9518B] text-white p-2 rounded-full"
+                            >
+                                <a href={link.href}>
+                                    <link.icon />
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
-            <div>
-                <h3>Services</h3>
-                <ul>
-                    {services.map((service) => (
-                        <li key={service}>{service}</li>
-                    ))}
-                </ul>
-            </div>
-            <div>
-                <form>
-                    <input type="text" placeholder="Enter text" />
-                    <Button
-                        onClick={() => console.log('Button clicked')}
-                        label="Submit"
-                    />
-                </form>
-                <div>
-                    <span>+2348063583111</span>
-                    <span>6 djibouti Street Wuse 2, FCT Abuja </span>
+                <div className="text-left">
+                    <h3 className="mb-4">Services</h3>
+                    <ul className="capitalize">
+                        {services.map((service) => (
+                            <li key={service}>{service}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="flex flex-col gap-4 text-left">
+                    <form>
+                        <h3 className="mb-4">Subscribe To Newsletter</h3>
+                        <input
+                            type="text"
+                            placeholder="Enter text"
+                            className="py-2 px-4 rounded-md mb-4"
+                        />
+                        <div>
+                            <Button
+                                onClick={() => console.log('Button clicked')}
+                                label="Submit"
+                            />
+                        </div>
+                    </form>
+                    <div>
+                        <li className="flex gap-2 items-baseline mb-4">
+                            <FaPhone />
+                            <span>+2348063583111</span>
+                        </li>
+                        <li className="flex gap-2 items-baseline">
+                            <FaLocationArrow />
+                            <span>6 djibouti Street Wuse 2, FCT Abuja </span>
+                        </li>
+                    </div>
                 </div>
             </div>
-            <div>
+            <div className="flex justify-between text-[#232323] pt-20">
                 <p>Copyright &copy; 2024 MevSpecialist Hospital Abuja </p>
                 <p>
                     <span>Terms of Use</span>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
         extend: {
@@ -17,5 +18,6 @@ export default {
             },
         },
     },
+    important: true,
     plugins: [],
 } satisfies Config;
