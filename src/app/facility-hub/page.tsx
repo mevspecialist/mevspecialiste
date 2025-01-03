e8031b803c353496892971f4c850648aab4d8923
firerum <ademuyiwaadewuyi@gmail.com>
2025-01-03 14:56:01 +0100

    facility hub wip

--- src/app/facility-hub/page.tsx
+++ src/app/facility-hub/page.tsx
@@ -1,4 +1,32 @@
 import React from 'react';
+import Image from 'next/image';
+
+const facilities: { title: string; content: string; image: string }[] = [
+    {
+        title: 'Operating Room',
+        content:
+            'Our state-of-the-art operating room is equipped with the latest medical technology and staffed by skilled surgeons and healthcare professionals. It’s a place where precision meets care, ensuring that patients receive the best surgical treatment available.',
+        image: '/images/facilityhub/facility-1.jpeg',
+    },
+    {
+        title: 'Reception',
+        content:
+            'The heart of our hospital, our reception area is your first point of contact when you visit us. Our friendly and well-trained receptionists are here to assist you with check-ins, appointments, and general inquiries.',
+        image: '/images/facilityhub/facility-2.jpeg',
+    },
+    {
+        title: 'Inpatient Ward',
+        content:
+            'Our inpatient ward is designed to provide round-the-clock care and comfort to patients requiring hospitalization. With spacious and well-appointed rooms, our priority is your well-being and recovery.',
+        image: '/images/facilityhub/facility-3.jpeg',
+    },
+    {
+        title: 'Ambulance',
+        content:
+            'At MEV Specialist hospital Abuja, Nigeria, we are committed to expanding our services to meet the growing healthcare needs of the community. Stay tuned for the upcoming addition of our state-of-the-art ambulance facility.',
+        image: '/images/facilityhub/facility-4.jpeg',
+    },
+];
 
 const FacilityHub: React.FC = () => {
     return (
@@ -16,7 +44,26 @@ const FacilityHub: React.FC = () => {
                     </p>
                 </div>
             </section>
-            <section className="py-20"></section>
+            <section className="py-20">
+                {facilities.map((facility, index) => (
+                    <div
+                        key={index}
+                        className="grid grid-cols-1 md:grid-cols-2 gap-6"
+                    >
+                        <div key={index} className="py-10">
+                            <h2>{facility.title}</h2>
+                            <p>{facility.content}</p>
+                        </div>
+                        <Image
+                            src={facility.image}
+                            alt={facility.title}
+                            width={500}
+                            height={300}
+                            className="rounded-3xl"
+                        />
+                    </div>
+                ))}
+            </section>
         </main>
     );
 };
