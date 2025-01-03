81ae061ee465e611251c683042f0065938b2e1e6
firerum <ademuyiwaadewuyi@gmail.com>
2025-01-03 11:52:40 +0100

    services wip

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -9,10 +9,14 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ title, content }) => {
     return (
         <div
             key={title}
-            className="px-10 py-10 text-xl bg-[#F8F8F8] rounded-xl hover:bg-hover-color hover:text-[#3C3C3C] transition-colors duration-200"
+            className="relative px-10 pb-8 pt-20 text-xl bg-[#F8F8F8] rounded-xl hover:bg-hover-color hover:text-[#3C3C3C] transition-colors duration-200"
         >
             <h3>{title}</h3>
             <p className="py-2 text-[13px] leading-5">{content}</p>
+            <span
+                aria-hidden="true"
+                className="inline-block w-10 h-10 bg-btn-color rounded-full absolute top-7"
+            ></span>
         </div>
     );
 };
