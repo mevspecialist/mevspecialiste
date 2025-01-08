6bd14bbd851eb21ad5a271fc1a92c31d62ab1800
firerum <ademuyiwaadewuyi@gmail.com>
2025-01-08 23:28:08 +0100

    remove comments

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: Request) {
         const transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
             port: 465,
-            secure: true, // Use true for 465, false for other ports
+            secure: true, // Use true for 465
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS,
@@ -45,4 +45,3 @@ export async function POST(req: Request) {
         );
     }
 }
-
