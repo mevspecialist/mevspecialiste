41e1ce35fdd2ae6691765d3c139f232a6eeb3545
firerum <ademuyiwaadewuyi@gmail.com>
2025-01-10 19:47:25 +0100

    star styling in service circle and paragragh

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -9,7 +9,7 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ title, content }) => {
     return (
         <div
             key={title}
-            className="relative px-10 pb-8 pt-20 text-xl bg-[#F8F8F8] rounded-xl hover:bg-hover-color hover:text-[#3C3C3C] transition-colors duration-200"
+            className="starwrapper relative px-10 pb-8 pt-20 text-xl bg-[#F8F8F8] rounded-xl hover:bg-hover-color hover:text-[#3C3C3C] transition-colors duration-200"
         >
             <h3>{title}</h3>
             <p className="py-2 text-[13px] leading-5">{content}</p>
